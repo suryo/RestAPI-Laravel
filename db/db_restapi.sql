92684936262501c368edb5d31760ee9757b8dd4c
suryo <suryoatm@gmail.com>
2024-03-04 20:49:02 +0700

    backup database

--- db/db_restapi.sql
+++ db/db_restapi.sql
@@ -11,7 +11,7 @@
  Target Server Version : 100422
  File Encoding         : 65001
 
- Date: 04/03/2024 19:03:29
+ Date: 04/03/2024 20:48:55
 */
 
 SET NAMES utf8mb4;
@@ -24,15 +24,22 @@ DROP TABLE IF EXISTS `tbl_category`;
 CREATE TABLE `tbl_category`  (
   `id` int NOT NULL AUTO_INCREMENT,
   `category` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
-  `status` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
+  `status` enum('active','inactive') CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT 'active',
   `created_at` datetime NULL DEFAULT NULL,
   `updated_at` datetime NULL DEFAULT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of tbl_category
 -- ----------------------------
+INSERT INTO `tbl_category` VALUES (1, 'sabun', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (2, 'pasta gigi', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (3, 'mie', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (4, 'bumbu masak', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (5, 'makanan kecil', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (6, 'minuman ringan', 'active', NULL, NULL);
+INSERT INTO `tbl_category` VALUES (7, 'skin care', 'active', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for tbl_product
